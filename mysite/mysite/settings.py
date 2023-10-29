4d2dbcb5ee31666af5731a1e8e47f7297fa65cb4
kuldeeptiwari0948 <kuldeeptiwari0948@gmail.com>
2023-10-29 22:42:57 +0530

    fifth

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,9 +25,8 @@ SECRET_KEY = 'django-insecure-$w&sfrtu_rlre)8=%9&#z10l=)_d%a%h@z^wmc*gv15m2oe@^e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'http://13.127.243.62:8000/'
-]
+ALLOWED_HOSTS = ["*"]
+
 
 
 # Application definition
